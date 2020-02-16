ff1921ab06ee12558e63bcd70e8f7b16bf7bb581
tOOlmaRR <georgeumarr@hotmail.com>
2020-02-16 16:22:57 -0500

    enable strict types mode
    update data import program to force strict typing so that expected return types and function parameter types are enforced, plus program updates to support this

--- InventoryImportTool/import.php
+++ InventoryImportTool/import.php
@@ -5,8 +5,9 @@
 * @author    George U. Marr
 */
 
-ini_set("display_errors", 1);
-ini_set("display_startup_errors", 1);
+declare(strict_types = 1);
+ini_set("display_errors", "1");
+ini_set("display_startup_errors", "1");
 error_reporting(-1);
 
 require 'vendor/autoload.php';
