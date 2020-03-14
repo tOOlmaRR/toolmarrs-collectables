5bb186d29c383d012b53ab1fa3a1ac6baca819f1
tOOlmaRR <georgeumarr@hotmail.com>
2020-03-14 09:53:22 -0400

    add a seeds query to seed the database with standard data
    this file is meant to be run on initial database creation

---
+++ Database/seeds.sql
@@ -0,0 +1,2 @@
+INSERT INTO `tsc2020-dev`.`attributes`(`Abbreviation`,`FullName`,`Comments`)
+VALUES('RC','Rookie Card','This card has been determined to be a rookie card through card appraisal sources such as Beckett');
