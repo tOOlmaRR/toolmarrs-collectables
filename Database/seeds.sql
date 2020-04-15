d590c214658651f746f980624d3c448a537863a9
tOOlmaRR <georgeumarr@hotmail.com>
2020-04-15 15:16:37 -0400

    add seed data for Sport and League
    just Hockey and NHL for now

--- Database/seeds.sql
+++ Database/seeds.sql
@@ -1,2 +1,8 @@
 INSERT INTO `tsc2020-dev`.`attributes`(`Abbreviation`,`FullName`,`Comments`)
 VALUES('RC','Rookie Card','This card has been determined to be a rookie card through card appraisal sources such as Beckett');
+
+INSERT INTO `tsc2020-dev`.`sport`(`Name`)
+VALUES('Hockey');
+
+INSERT INTO `tsc2020-dev`.`league`(`Abbreviation`, `Name`, `Sport_ID`)
+VALUES('NHL', 'National Hockey League', LAST_INSERT_ID());
