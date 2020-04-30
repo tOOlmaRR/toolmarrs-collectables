06ae77ab5d41586a70d346d9223f26bec3cdeea0
tOOlmaRR <georgeumarr@hotmail.com>
2020-04-30 12:17:03 -0400

    add a DateTimeHelper function to return a DB-friendly timestamp string
    a new method that takes as input a string and returns a value that should be friendly for insertion into a database column of time DateTime. If the input string isn't a valid timestamp, return a NULL. The output format is Y-m-d.

--- InventoryImportTool/src/helpers/dateTimeHelpers.php
+++ InventoryImportTool/src/helpers/dateTimeHelpers.php
@@ -6,9 +6,23 @@ class DateTimeHelpers
     /**
     * Determine if a given datetime string in a given format is valid
     */
-    public static function validateDateTime($dateString, $dateFormat) : bool
+    public static function validateDateTime($datetimeString, $datetimeFormat) : bool
     {
-        $dateTimeObject = \DateTime::createFromFormat($dateFormat, $dateString);
-        return $dateTimeObject && $dateTimeObject->format($dateFormat) == $dateString;
+        $datetimeObject = \DateTime::createFromFormat($datetimeFormat, $datetimeString);
+        return $datetimeObject && $datetimeObject->format($datetimeFormat) == $datetimeString;
+    }
+    
+    /**
+    * Given a string, return a string in a format that can be inserted into a DateTime column in a database, assuming the column is nullable
+    */
+    public static function convertToDbFriendlyString($datetimeString, $expectedinputFormat) : ?string {
+        if (is_null($datetimeString)) {
+            return null;
+        } elseif (DateTimeHelpers::validateDateTime($datetimeString, $expectedinputFormat)) {
+            $datetimeObject = \DateTime::createFromFormat($expectedinputFormat, $datetimeString);
+            return $datetimeObject->format("Y-m-d");
+        } else {
+            return null;
+        }
     }
 }
