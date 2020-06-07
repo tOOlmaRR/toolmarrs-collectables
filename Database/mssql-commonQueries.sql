3f7794ddb3bbf0a150c089b33373b0efe3711f98
tOOlmaRR <georgeumarr@hotmail.com>
2020-06-07 11:58:26 -0400

    simplified MS SQL queries
    reverted all queries to be database-agnostic so that the database can be defined in a single use statement at the top and be run against any db instance with only one change

--- Database/mssql-commonQueries.sql
+++ Database/mssql-commonQueries.sql
@@ -1,30 +1,33 @@
+-- which database are we connecting to?
+use [GeosTradingCards]
+
 -- select *
-SELECT * FROM [GeosTradingCards].[dbo].[manufacturer];
-SELECT * FROM [GeosTradingCards].[dbo].[cardset];
-SELECT * FROM [GeosTradingCards].[dbo].[card];
-SELECT * FROM [GeosTradingCards].[dbo].[card_has_attributes];
-SELECT * FROM [GeosTradingCards].[dbo].[attributes];
-SELECT * FROM [GeosTradingCards].[dbo].[playerposition];
-SELECT * FROM [GeosTradingCards].[dbo].[team];
-SELECT * FROM [GeosTradingCards].[dbo].[cardvalue];
-SELECT * FROM [GeosTradingCards].[dbo].[subset];
-SELECT * FROM [GeosTradingCards].[dbo].[singlecard];
-SELECT * FROM [GeosTradingCards].[dbo].[singlecardgrading];
-SELECT * FROM [GeosTradingCards].[dbo].[gradingclass];
+SELECT * FROM [dbo].[manufacturer];
+SELECT * FROM [dbo].[cardset];
+SELECT * FROM [dbo].[card];
+SELECT * FROM [dbo].[card_has_attributes];
+SELECT * FROM [dbo].[attributes];
+SELECT * FROM [dbo].[playerposition];
+SELECT * FROM [dbo].[team];
+SELECT * FROM [dbo].[cardvalue];
+SELECT * FROM [dbo].[subset];
+SELECT * FROM [dbo].[singlecard];
+SELECT * FROM [dbo].[singlecardgrading];
+SELECT * FROM [dbo].[gradingclass];
 
 -- delete *
-delete from [GeosTradingCards].[dbo].[card_has_attributes] where Card_ID < 1000;
-delete from [GeosTradingCards].[dbo].[attributes] where ID > 1;
-delete from [GeosTradingCards].[dbo].[cardvalue] where ID >= 1;
-delete from [GeosTradingCards].[dbo].[singlecardgrading] where ID >= 1;
-delete from [GeosTradingCards].[dbo].[gradingclass] where ID >= 1;
-delete from [GeosTradingCards].[dbo].[singlecard] where ID <> '';
-delete from [GeosTradingCards].[dbo].[card] where id < 1000;
-delete from [GeosTradingCards].[dbo].[subset] where ID >= 1;
-delete from [GeosTradingCards].[dbo].[team] where ID >= 1;
-delete from [GeosTradingCards].[dbo].[playerposition] where ID >= 1;
-delete from [GeosTradingCards].[dbo].[cardset] where id < 10;
-delete from [GeosTradingCards].[dbo].[manufacturer] where id < 10;
+delete from [dbo].[card_has_attributes] where Card_ID < 1000;
+delete from [dbo].[attributes] where ID > 1;
+delete from [dbo].[cardvalue] where ID >= 1;
+delete from [dbo].[singlecardgrading] where ID >= 1;
+delete from [dbo].[gradingclass] where ID >= 1;
+delete from [dbo].[singlecard] where ID <> '';
+delete from [dbo].[card] where id < 1000;
+delete from [dbo].[subset] where ID >= 1;
+delete from [dbo].[team] where ID >= 1;
+delete from [dbo].[playerposition] where ID >= 1;
+delete from [dbo].[cardset] where id < 10;
+delete from [dbo].[manufacturer] where id < 10;
 
 -- reset autoincrement indexes
 DBCC CHECKIDENT([attributes], RESEED, 1)
@@ -41,15 +44,15 @@ DBCC CHECKIDENT([singlecardgrading], RESEED, 0)
 -- card set details
 select [cardset].Season, manufacturer.Name, [cardset].BaseSetName, [cardset].InsertSetName, [cardset].FullName, [cardset].Size, [cardset].Rarity,
 	[cardset].GradingModifier, [cardset].Comments, [cardset].LastInventoryCheck
-from [GeosTradingCards].[dbo].[cardset] with (nolock)
-left join [GeosTradingCards].[dbo].[manufacturer] with (nolock)
+from [dbo].[cardset] with (nolock)
+left join [dbo].[manufacturer] with (nolock)
 on cardset.[Manufacturer_ID] = [manufacturer].ID
 order by season
 
--- cards grid STOPPED HERE
+-- cards grid
 select [card].CardNumber, [card].Title, Attributes.Abbreviation, [team].Name, [playerposition].Abbreviation, [cardvalue].LowValue, [cardvalue].HighValue, Subset.Name, Card.GradingModifier, 
 	[cardvalue].LastAppraisal, [cardvalue].LastAppraisalSource
-from [GeosTradingCards].[dbo].[card] with (nolock)
+from [dbo].[card] with (nolock)
 left join [GeosTradingCards].[dbo].[card_has_attributes] on [card_has_attributes].Card_ID = [card].ID
 left join [GeosTradingCards].[dbo].[attributes] on Attributes.ID = [card_has_attributes].Attributes_ID
 left join [GeosTradingCards].[dbo].[team] on Team.ID = [card].Team_ID
@@ -66,4 +69,3 @@ left join [GeosTradingCards].[dbo].[singlecardgrading] on [singlecardgrading].Si
 left join [GeosTradingCards].[dbo].[gradingclass] on [gradingclass].ID = [singlecardgrading].GradingClass_ID
 where [singlecard].Card_ID = 1
 order by ID
-
